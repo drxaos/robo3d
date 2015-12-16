ab0325d9453e7eb9dc326667e948cc2c160b04ba
xaos <xaos@z-inc.net>
2015-12-16 20:49:43 +0500

    map

--- src/main/resources/Tiles/tileset.sh
+++ src/main/resources/Tiles/tileset.sh
@@ -1,6 +1,6 @@
-montage `ls walls-*.png` -geometry 50x50 -tile 5x tileset-walls.png
-montage `ls doors-*.png` -geometry 50x50 -tile 5x tileset-doors.png
-montage `ls roads-*.png` -geometry 50x50 -tile 5x tileset-roads.png
-montage `ls roofs-*.png` -geometry 50x50 -tile 5x tileset-roofs.png
+montage `ls roads-*.png` -geometry 50x50 -tile 5x -background '#ffffff' tileset-roads.png
+montage `ls walls-*.png` -geometry 50x50 -tile 5x -background '#00ff00' tileset-walls.png
+montage `ls doors-*.png` -geometry 50x50 -tile 5x -background '#ffff00' tileset-doors.png
+montage `ls roofs-*.png` -geometry 50x50 -tile 5x -background '#000000' tileset-roofs.png
 
 
